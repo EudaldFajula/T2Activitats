354115b92e4913f55d4da0940ae7b286092308e4
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2024-11-18 20:06:54 +0100

    Act 16. Closes #17

--- Program.cs
+++ Program.cs
@@ -1,34 +1,43 @@
 ﻿using System;
-using System.Net.NetworkInformation;
 
-
-//Pre: The user need to enter a decimal number
-namespace FajulaEudaldEXTfifteen
+//Pre: The user need to enter a roman numerals
+namespace FajulaEudaldEXTsixteen
 {
-    public class Exercicififteen
+    public class Exercicisixteen
     {
-        public static decimal TransformDecimalpart(decimal number)
+        public static int RomeNumToNum(string romeNum)
         {
-            int aux = (int)number;
-            decimal decimalNum = number - aux;
-            return decimalNum;
-        }
-        public static int TransformEnter(decimal number)
-        {
-            return ((int)number);
+            int normalNum = 0;
+            for (int i = romeNum.Length - 1; i >= 0; i--)
+            {
+                /*Mira quins caracter hi ha en la posicio, depenen del caracter fa una cosa o l'altra.
+                IMPORTANT els numero romans que nomes resten son el C = 100, X = 10, I = 1. (Crec que hi han mes)*/
+                switch (romeNum[i])
+                {
+                    case 'M': normalNum += 1000; break;
+                    case 'D': normalNum += 500; break;
+                    //Cas C = 100. Si hi ha una M = 1000 devant resta o si hi ha una D = 500 tambe resta
+                    case 'C': normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == 'M' || romeNum[i + 1] == 'D') ? -100 : 100); break;
+                    case 'L': normalNum += 50; break;
+                    //Cas X = 10. Si hi ha una C= 100 devant resta o si hi ha una L = 50 tambe resta
+                    case 'X': normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == 'C' || romeNum[i + 1] == 'L') ? -10 : 10); break;
+                    case 'V': normalNum += 5; break;
+                    //Cas I = 1. Si hi ha una X = 10 devant resta o si hi ha una V = 5 tambe resta
+                    case 'I': normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == 'X' || romeNum[i + 1] == 'V') ? -1 : 1); break;
+                }
+
+            }
+            return normalNum;
         }
         public static void Main()
         {
-            const string Msg = "Introdueix un numero decimal: ";
-            const string EnterMsg = "El numero en enter es: {0}";
-            const string DecimalMsg = "Els decimals son: {0}";
-            decimal number;
+            const string Msg = "Introdueix un numero romà: ";
+            const string FinalMsg = "El teu número en nombres és: {0}";
             Console.WriteLine(Msg);
             try
             {
-                number = Convert.ToDecimal(Console.ReadLine());
-                Console .WriteLine(EnterMsg, TransformEnter(number));
-                Console.WriteLine(DecimalMsg, TransformDecimalpart(number));
+                string romeNum = Console.ReadLine();
+                Console.WriteLine(FinalMsg, RomeNumToNum(romeNum));
             }
             catch (Exception)
             {
@@ -36,5 +45,5 @@ namespace FajulaEudaldEXTfifteen
             }
         }
     }
-}   
-//Post: The user will know the number he entered the enter part and the decimal part
+}
+//Post: The user will know the roman number in normal numbers
