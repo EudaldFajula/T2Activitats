b00f2b80694384fb8e0ef12430217dc63adc52f3
Eudald Fajula <eudald.fajula.7e6@itb.cat>
2024-11-20 10:21:36 +0100

    Update Act16

--- Program.cs
+++ Program.cs
@@ -7,23 +7,25 @@ namespace FajulaEudaldEXTsixteen
     {
         public static int RomeNumToNum(string romeNum)
         {
+            const char RomanCharM = 'M', RomanCharD = 'D', RomanCharC = 'C', RomanCharL = 'L', RomanCharX = 'X', RomanCharV = 'V', RomanCharI = 'I'; 
             int normalNum = 0;
+            romeNum = romeNum.ToUpper();
             for (int i = romeNum.Length - 1; i >= 0; i--)
             {
                 /*Mira quins caracter hi ha en la posicio, depenen del caracter fa una cosa o l'altra.
                 IMPORTANT els numero romans que nomes resten son el C = 100, X = 10, I = 1. (Crec que hi han mes)*/
                 switch (romeNum[i])
                 {
-                    case 'M': normalNum += 1000; break;
-                    case 'D': normalNum += 500; break;
+                    case RomanCharM: normalNum += 1000; break;
+                    case RomanCharD: normalNum += 500; break;
                     //Cas C = 100. Si hi ha una M = 1000 devant resta o si hi ha una D = 500 tambe resta
-                    case 'C': normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == 'M' || romeNum[i + 1] == 'D') ? -100 : 100); break;
-                    case 'L': normalNum += 50; break;
+                    case RomanCharC: normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == RomanCharM || romeNum[i + 1] == RomanCharD) ? -100 : 100); break;
+                    case RomanCharL: normalNum += 50; break;
                     //Cas X = 10. Si hi ha una C= 100 devant resta o si hi ha una L = 50 tambe resta
-                    case 'X': normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == 'C' || romeNum[i + 1] == 'L') ? -10 : 10); break;
-                    case 'V': normalNum += 5; break;
+                    case RomanCharX: normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == RomanCharC || romeNum[i + 1] == RomanCharL) ? -10 : 10); break;
+                    case RomanCharV: normalNum += 5; break;
                     //Cas I = 1. Si hi ha una X = 10 devant resta o si hi ha una V = 5 tambe resta
-                    case 'I': normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == 'X' || romeNum[i + 1] == 'V') ? -1 : 1); break;
+                    case RomanCharI: normalNum += (i < romeNum.Length - 1 && (romeNum[i + 1] == RomanCharX || romeNum[i + 1] == RomanCharV) ? -1 : 1); break;
                 }
 
             }
